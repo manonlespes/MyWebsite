616006b79eb100202f477bf72dd0117a697152c3
Manon Lespes <manonlespes@hotmail.fr>
2021-11-19 16:11:42 +0100

    work on the menu shape

--- view/menu/menu.php
+++ view/menu/menu.php
@@ -1,3 +1,13 @@
-<div>
-    ici c'est le menu
-</div>
+<nav class="navBar">
+    <ul class="menu">
+        <div class="list">
+            <li><a href="#">X</a></li><!-- TODO mettre un favicon fermé -->
+            <li><a href="#">Experiences</a></li>
+            <li><a href="#">Backgrounds</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">Languages</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </div>
+    </ul>
+</nav>
