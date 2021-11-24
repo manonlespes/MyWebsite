b80f5dc47008fc359706db4c1f7115a7b790cb2e
Manon Lespes <manonlespes@hotmail.fr>
2021-11-24 16:26:54 +0100

    work on the responsive tablet & desktop screens

--- view/page/myCV.php
+++ view/page/myCV.php
@@ -5,7 +5,7 @@
 
 <section class="introduction">
 
-   <!--  <div class="creativity">
+    <!--  <div class="creativity">
         <h2>Creativity</h2>
         <div>Innovative woman who enjoys exploring different point of views</div>
     </div>
@@ -15,21 +15,22 @@
         <div>Junior frontend developer who loves coding and taking on many challenges</div>
     </div> -->
     <div class="images_intro">
+
         <div class="web_safe">
             <img src="public/images/web_safe.png" alt="web_icon">
         </div>
-    
+
         <div class="image_Manon">
             <img src="public/images/Fusion_transparent-V2.png" alt="image_ManonLespes">
         </div>
+
     </div>
 
     <div class="text_intro">
         <h1>Hey You, Welcome on my Portofolio !</h1>
 
-        <h3>My name is Manon.<br> If you have a question or an internship position to offer, drop me a line, I'd be delighted to hear from you !
-        </h3>
+        <p>My name is Manon.<br> If you have a question or an internship position to offer, drop me a line ! I'd be delighted to hear from you.
+        </p>
     </div>
-
 
 </section>
