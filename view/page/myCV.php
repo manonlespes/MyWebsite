56119d15c3cd4e159b8c9c3303692a3aa7384ada
Manon Lespes <manonlespes@hotmail.fr>
2021-11-24 15:32:25 +0100

    work on the banner mobile-first

--- view/page/myCV.php
+++ view/page/myCV.php
@@ -1,1 +1,35 @@
-<h1>Hello World</h1>
+<div class="logo">
+    <a href="#"><img src="public/images/Logo.png" alt="Logo"></a> <!-- TODO set an ancre -->
+</div>
+
+
+<section class="introduction">
+
+   <!--  <div class="creativity">
+        <h2>Creativity</h2>
+        <div>Innovative woman who enjoys exploring different point of views</div>
+    </div>
+
+    <div class="code">
+        <h2>&lt;Code&gt;</h2>
+        <div>Junior frontend developer who loves coding and taking on many challenges</div>
+    </div> -->
+    <div class="images_intro">
+        <div class="web_safe">
+            <img src="public/images/web_safe.png" alt="web_icon">
+        </div>
+    
+        <div class="image_Manon">
+            <img src="public/images/Fusion_transparent-V2.png" alt="image_ManonLespes">
+        </div>
+    </div>
+
+    <div class="text_intro">
+        <h1>Hey You, Welcome on my Portofolio !</h1>
+
+        <h3>My name is Manon.<br> If you have a question or an internship position to offer, drop me a line, I'd be delighted to hear from you !
+        </h3>
+    </div>
+
+
+</section>
