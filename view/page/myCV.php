121364b29dae3d9ef50c700fd254e3b0a9d881c8
Manon Lespes <manonlespes@hotmail.fr>
2021-11-26 17:00:59 +0100

    end of the mobile breakpoint (default)

--- view/page/myCV.php
+++ view/page/myCV.php
@@ -52,20 +52,90 @@
         <div class="exp2"> <!-- flex-flow: row -->
             <div class="container_flex"><!-- flex-flow:column -->
                 <div>
-                    <p>Membership & Event coordinator</p>
-                    <p>Project Coordinator</p>
+                    <p>Assistant Project Manager</p>
+                    <p>Junior Community Manager</p>
                 </div>
     
                 <div class="subtext">
-                    <p>World Employment Confederation</p>
+                    <p>Downtown Europe</p>
                     <p>Brussels, Belgium <span class="more">SEE MORE +</span></p>
                 </div>
             </div>
 
             <div class="tag_exp">
-                <p>01/2020-12/2020</p>
+                <p>08/2018-10/2019</p>
             </div>
         </div>
+
+        <div class="exp3"> <!-- flex-flow: row -->
+            <div class="container_flex"><!-- flex-flow:column -->
+                <div>
+                    <p>Hostess</p>
+                </div>
+    
+                <div class="subtext">
+                    <p>Downtown Europe</p>
+                    <p>Brussels, Belgium <span class="more">SEE MORE +</span></p>
+                </div>
+            </div>
+
+            <div class="tag_exp">
+                <p>2014-2018</p>
+            </div>
+        </div>
+
+        <div class="exp4"> <!-- flex-flow: row -->
+            <div class="container_flex"><!-- flex-flow:column -->
+                <div>
+                    <p>Intern - Project Coordinator</p>
+                </div>
+    
+                <div class="subtext">
+                    <p>Ciudad Alégrià</p>
+                    <p>Cancùn, Mexico <span class="more">SEE MORE +</span></p>
+                </div>
+            </div>
+
+            <div class="tag_exp">
+                <p>08/2017-10/2017</p>
+            </div>
+        </div>
+
+        <div class="exp5"> <!-- flex-flow: row -->
+            <div class="container_flex"><!-- flex-flow:column -->
+                <div>
+                    <p>Intern - Frontdesk Officer</p>
+                </div>
+    
+                <div class="subtext">
+                    <p>Camping de la Côte Sauvage Ecolabel</p>
+                    <p>Saint-Clément-des-Baleines, France <span class="more">SEE MORE +</span></p>
+                </div>
+            </div>
+
+            <div class="tag_exp">
+                <p>06/2013-09/2013</p>
+            </div>
+        </div>
+
+        <div class="exp6"> <!-- flex-flow: row -->
+            <div class="container_flex"><!-- flex-flow:column -->
+                <div>
+                    <p>Intern - Journalist/Editor</p>
+                </div>
+    
+                <div class="subtext">
+                    <p>Gazette de Berlin</p>
+                    <p>Berlin, Germany <span class="more">SEE MORE +</span></p>
+                </div>
+            </div>
+
+            <div class="tag_exp">
+                <p>06/2012-09/2012</p>
+            </div>
+        </div>
+
+        
 
     </div>
 </section>
