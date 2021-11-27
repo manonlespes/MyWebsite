eb32776dd3d8c4b1deefcdea5d2d6edbb5e7ed87
Manon Lespes <manonlespes@hotmail.fr>
2021-11-27 20:04:46 +0100

    work on the backgrounds div - responsive mobile

--- view/menu/menu.php
+++ view/menu/menu.php
@@ -5,7 +5,7 @@
                 <img src="public/images/cross_closed.png" alt="cross_closed">
             </li> <!-- TODO make it visible only on click -->
             <li><a href="#experiences">Experiences</a></li>
-            <li><a href="#">Backgrounds</a></li>
+            <li><a href="#backgrounds">Backgrounds</a></li>
             <li><a href="#">Skills</a></li>
             <li><a href="#">Languages</a></li>
             <li><a href="#">Projects</a></li>
--- view/page/myCV.php
+++ view/page/myCV.php
@@ -103,7 +103,7 @@
                 </div>
 
                 <div class="subtext">
-                    <p><a href="https://www.ciudadalegria.org.mx/" target="_blank" rel="noopener noreferrer">Fundación  Ciudad de la Alégrià</a></p>
+                    <p><a href="https://www.ciudadalegria.org.mx/" target="_blank" rel="noopener noreferrer">Fundación Ciudad de la Alégrià</a></p>
                     <p>Cancùn, Mexico <span class="more">SEE MORE +</span></p>
                 </div>
             </div>
@@ -150,17 +150,159 @@
                 <p>06/2012-09/2012</p>
             </div>
         </div>
-
-
-
     </div>
 </section>
 
-<section style="height: 100px;">
+<section id="backgrounds" class="backgrounds">
+
+    <div class="title2">
+        <img src="public/images/title_background.png" alt="backgrounds">
+    </div>
+
+    <div class="container_back">
+        <div class="back1">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back1_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Webmaster - Front-End Developer</p>
+                </div>
+
+                <div class="subtext">
+                    <p><a href="https://www.interface3.be/fr/formation/webmaster" target="_blank" rel="noopener noreferrer">Interface3</a></p>
+                    <p>Brussels, Belgium</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="back2">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back2_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Assertivity as fundamental tool of communication at work</p>
+                </div>
+
+                <div class="subtext">
+                    <p><a href="https://www.bruxellesformation.brussels/catalogue-bf-entreprises/lassertivite-comme-outil-de-travail/" target="_blank" rel="noopener noreferrer">Bruxelles Formation</a></p>
+                    <p>Brussels, Belgium</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="back3">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back3_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Master in Multilingual Communication</p>
+                </div>
+
+                <div class="subtext">
+                    <p><a href="https://www.ulb.be/en/programme/ma-babl#programme" target="_blank" rel="noopener noreferrer">Minor Multiculturality</a></p>
+                    <p>Languages EN, ES, DE, FR</p>
+                    <p><a href="https://www.ulb.be/en/programme/ma-babl" target="_blank" rel="noopener noreferrer">ULB</a></p>
+                    <p>Brussels, Belgium</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="back4">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back4_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Bachelor degree in Translation</p>
+                </div>
+
+                <div class="subtext">
+                    <p>Erasmus Dublin (2014) at UCD - 3 months</p>
+                    <p>Languages EN, DE, FR</p>
+                    <p><a href="https://ltc.ulb.be/nos-departements-d-enseignement/ecole-de-traduction-et-interpretation-isti-cooremans-1" target="_blank" rel="noopener noreferrer">ISTI</a></p>
+                    <p>Brussels, Belgium</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="back5">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back5_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Bachelor degree in Foreign Languages</p>
+                </div>
+
+                <div class="subtext">
+                    <p>Languages EN, DE, FR</p>
+                    <p><a href="https://www.ict-toulouse.fr/formation/licence-langues-etrangeres-appliquees/" target="_blank" rel="noopener noreferrer">ICT</a> </p>
+                    <p>Toulouse, France</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="back6">
+            <!-- flex-flow: row -->
+            <picture class="tag_back">
+                <source media="(min-width:700px)" srcset="public/images.png">
+                <source media="(min-width:960px)" srcset="public/images/.png">
+                <img src="public/images/pen_back6_small.png" alt="image_ManonLespes"><!-- default -->
+            </picture>
+
+            <div class="container_flex">
+                <!-- flex-flow:column -->
+                <div>
+                    <p>Baccalaureate Literature</p>
+                </div>
+
+                <div class="subtext">
+                    <p>Minor German</p>
+                    <p> <a href="https://saint-sernin.mon-ent-occitanie.fr/" target="_blank" rel="noopener noreferrer">Lycée Saint-Sernin</a></p>
+                    <p>Toulouse, France</p>
+                </div>
+            </div>
+        </div>
 
 </section>
 
-<section>
+<section style="height:100px">
 
 </section>
-
