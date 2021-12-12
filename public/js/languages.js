0816f5fef03bc5bfc2018bad88d2f2a9f123ef78
Manon Lespes <manonlespes@hotmail.fr>
2021-12-12 16:07:06 +0100

    work on the js of the section languages

--- public/js/languages.js
+++ public/js/languages.js
@@ -22,26 +22,105 @@ english.addEventListener("click", function () {
     /* english.innerHTML = 'X' */
 
     const h3 = document.createElement('h3');
+    h3.setAttribute("id","closeLanguage");
+    
     textLanguage.appendChild(h3);
     h3.innerHTML = 'English';
-
+    
     const p = document.createElement('p');  
     textLanguage.appendChild(p);
     p.innerHTML = `Hello there! <br> My English is pretty good. According to the Common European Framework of Reference for Languages, my level is C1.`;
-
+    
 });
+
+/* const closeLanguage = document.getElementById("closeLanguage");
+
+closeLanguage.addEventListener("click", function(){
+    hideBubble.classList.toggle("hide-bubble");
+
+   if ((french && german && spanish && english).classList.contains("hide-bubble")) {
+        english.classList.remove("hide-bubble")
+        french.classList.remove("hide-bubble")
+        german.classList.remove("hide-bubble")
+        spanish.classList.remove("hide-bubble")
+    }
+
+}) */
+
 
 french.addEventListener("click", function () {
     hideBubble.classList.toggle("hide-bubble");
+
+    if (!(french && german && spanish && english).classList.contains("hide-bubble")) {
+        english.classList.add("hide-bubble")
+        french.classList.add("hide-bubble")
+        german.classList.add("hide-bubble")
+        spanish.classList.add("hide-bubble")
+    }
+
+    /* french.innerHTML = 'X' */
+
+    const h3 = document.createElement('h3');
+    textLanguage.appendChild(h3);
+    h3.setAttribute("id","closeLanguage");
+    const closeLanguage = document.getElementById("closeLanguage");
+
+    h3.innerHTML = 'French';
+
+    const p = document.createElement('p');  
+    textLanguage.appendChild(p);
+    p.innerHTML = `Bonjour ! <br> French is my mother tongue. Je peux donc facilement m'adapter à différents environnements linguistique.`;
 
 });
 
 german.addEventListener("click", function () {
     hideBubble.classList.toggle("hide-bubble");
 
+    if (!(french && german && spanish && english).classList.contains("hide-bubble")) {
+        english.classList.add("hide-bubble")
+        french.classList.add("hide-bubble")
+        german.classList.add("hide-bubble")
+        spanish.classList.add("hide-bubble")
+    }
+
+    /* german.innerHTML = 'X' */
+
+    const h3 = document.createElement('h3');
+    textLanguage.appendChild(h3);
+    h3.setAttribute("id","closeLanguage");
+    const closeLanguage = document.getElementById("closeLanguage");
+
+    h3.innerHTML = 'German';
+
+    const p = document.createElement('p');  
+    textLanguage.appendChild(p);
+    p.innerHTML = `Hallo! <br> Deutsch ist meine Lieblingssprache. Ich würde gerne mehr auf Deutsch sprechen, aber in einem beruflichen Kontext hatte ich noch nie die Gelegenheit, es zu verwenden (level B1/B2).`;
+
 });
 
 spanish.addEventListener("click", function () {
     hideBubble.classList.toggle("hide-bubble");
 
+    if (!(french && german && spanish && english).classList.contains("hide-bubble")) {
+        english.classList.add("hide-bubble")
+        french.classList.add("hide-bubble")
+        german.classList.add("hide-bubble")
+        spanish.classList.add("hide-bubble")
+    }
+
+    /* spanish.innerHTML = 'X' */
+
+    const h3 = document.createElement('h3');
+    textLanguage.appendChild(h3);
+    h3.setAttribute("id","closeLanguage");
+    const closeLanguage = document.getElementById("closeLanguage");
+
+    h3.innerHTML = 'Spanish';
+
+    const p = document.createElement('p');  
+    textLanguage.appendChild(p);
+    p.innerHTML = `Porque aprendí español en la universidad y en México. Lo entiendo/leo más que lo hablo, pero si es necesario estoy lista a practicar más el idioma (level A2/B1).`;
+
 });
+
+
