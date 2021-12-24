9322bee7b643f8c125deddddebfe067dd1a09095
Manon Lespes <manonlespes@hotmail.fr>
2021-12-24 18:22:52 +0100

    work on the gauge events fill 90% and 70% issue: delete a class when the other actived

--- public/js/gauge.js
+++ public/js/gauge.js
@@ -1,2 +1,85 @@
 'use strict';
 
+const gaugeFill = document.getElementById("gaugeFill");
+
+// events for skills estimated at 90% - fill comfortable with
+const html = document.getElementById("html");
+const illustrator = document.getElementById("illustrator");
+const xd = document.getElementById("xd");
+const vsCode = document.getElementById("vsCode");
+
+
+html.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill90")){
+        gaugeFill.classList.add("gaugeFill90")
+    }
+});
+
+illustrator.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill90")){
+        gaugeFill.classList.add("gaugeFill90")
+    }
+});
+
+xd.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill90")){
+        gaugeFill.classList.add("gaugeFill90")
+    }
+});
+
+vsCode.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill90")){
+        gaugeFill.classList.add("gaugeFill90")
+    }
+});
+
+
+
+// events for skills estimated at 70% - fill quite comfortable with
+
+const css = document.getElementById("css");
+const sass = document.getElementById("sass");
+const bootstrap = document.getElementById("bootstrap");
+const wordpress = document.getElementById("wordpress");
+
+css.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill70") ){
+        gaugeFill.classList.add("gaugeFill70")
+    }
+});
+
+bootstrap.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill70")){
+        gaugeFill.classList.add("gaugeFill70")
+    }
+});
+
+wordpress.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill70")){
+        gaugeFill.classList.add("gaugeFill70")
+    }
+});
+
+sass.addEventListener("click", function (){
+    gaugeFill.classList.toggle("gaugeFillHidden");
+    
+    if (!(gaugeFill).classList.contains("gaugeFill70")){
+        gaugeFill.classList.add("gaugeFill70")
+    }
+});
+
+
