686f4900c02a5408e817fb7a3c64f52ae4fb7f95
Manon Lespes <manonlespes@hotmail.fr>
2021-12-29 18:30:55 +0100

    work on the 404 - not finished yet (only small screen)

--- controller/404Controller.php
+++ controller/404Controller.php
@@ -1,5 +1,8 @@
 <?php
 
+include("view/html/header.php");
+include("controller/menuController.php"); 
 include("view/error/404.php");
+include("view/html/footer.php");
 
 ?>
--- view/error/404.php
+++ view/error/404.php
@@ -1,16 +1,25 @@
-<div>
-    <div>
-        <h2>We looked everywhere for this page</h2>
-        <h3>Error 404</h3>
-        <p>Sorry, Chatshimi (#alwaysBlameMyCat) may have walk on my computer... </p>
-        <p>But maybe, we can still help you to find what you are looking for: </p>
-    </div>
+<section>
+    <h2>We looked everywhere for this page</h2>
 
-    <div>
-        <picture>
-            <source media="(min-width:700px)" srcset="public/images/404/Chatshimi_medium.png">
-            <source media="(min-width:960px)" srcset="public/images/404/Chatshimi_large.png">
-            <img src="public/images/404/Chatshimi_small.png" title="Oops, I did it again!" alt="image_404_Chatshimi"><!-- default -->
-        </picture>
+    <picture>
+        <source media="(min-width:700px)" srcset="public/images/404/Chatshimi_medium.png">
+        <source media="(min-width:960px)" srcset="public/images/404/Chatshimi_large.png">
+        <img src="public/images/404/Chatshimi_small.png" title="Oops, I did it again!" alt="image_404_Chatshimi"><!-- default -->
+    </picture>
+
+    <div class="row">
+        <div class="text">
+            <h3>Error 404</h3>
+            <p>Sorry, Chatshimi (#alwaysBlameMyCat) may have walked on my computer... </p>
+            <p>But maybe, we can still help you to find what you are looking for: </p>
+            
+            <ul>
+                <li>Home Page</li>
+                <li>Experiences</li>
+                <li>Backgrounds</li>
+                <li></li>
+            </ul>
+        
+        </div>
     </div>
-</div>
+</section>
