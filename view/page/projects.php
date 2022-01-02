a113d524d3acc1a3b3b8da4b007185d7987c5f8d
Manon Lespes <manonlespes@hotmail.fr>
2022-01-02 18:04:24 +0100

    addition of images and final touches of responsive

--- view/page/projects.php
+++ view/page/projects.php
@@ -1,33 +1,77 @@
 <section class="drawings">
-    <h2> Projects - Drawings</h2>
+    <h2 class="h2"> Projects - Drawings</h2>
 
+    <h3>Projects made within the training courses</h3>
     <div class="gallery">
         <div class="border">
+            <!-- TODO check the target link -->
             <a target="_blank" href="Carte_postale_1.png">
-                <img src="public/images/gallery/Carte_postale_1.png" title="" alt="Cinque Terre">
+                <img src="public/images/gallery/Carte_postale_1.png" title="Sunset" alt="image-of-a-sunset">
             </a>
             <div class="desc">Design a postal card for <a href="https://www.interface3.be/" target="_blank" rel="noopener noreferrer">Interface3</a> while on summer holidays. <br> Done with Adobe Illustrator.</div>
         </div>
 
         <div class="border">
             <a target="_blank" href="Carte_vacances_2.png">
-                <img src="public/images/gallery/Carte_vacances_2.png" title="" alt="Forest">
+                <img src="public/images/gallery/Carte_vacances_2.png" title="Chilling hard" alt="image-chilling-holidays">
             </a>
-            <div class="desc">Design a postal card for <a href="https://www.interface3.be/" target="_blank" rel="noopener noreferrer">Interface3</a>  while on summer holidays and as I had so much fun, I draw another one. <br> Done with Adobe Illustrator.</div>
-        </div>
-
-        <div class="border">
-            <a target="_blank" href="Carte_postale_1.png">
-                <img src="public/images/gallery/Carte_postale_1.png" title="" alt="Cinque Terre">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="border">
-            <a target="_blank" href="Carte_vacances_2.png">
-                <img src="public/images/gallery/Carte_vacances_2.png" title="" alt="Forest">
-            </a>
-            <div class="desc">Add a description of the image here</div>
+            <div class="desc">Design a postal card for <a href="https://www.interface3.be/" target="_blank" rel="noopener noreferrer">Interface3</a> while on summer holidays and as I had so much fun, I drew another one. <br> Done with Adobe Illustrator.</div>
         </div>
     </div>
+
+    <h3>Personal drawings</h3>
+
+    <!-- <picture>
+                <source media="(min-width:700px)" srcset="public/images/medium_screen_speak.png">
+                <source media="(min-width:960px)" srcset="public/images/large_screen_speak.png">
+                <img src="public/images/small_screen_speak.png" alt="experiences">
+            </picture> -->
+
+    <div class="gallery2">
+
+        <div class="border">
+            <a target="_blank" href="Eye_cat_small.jpg">
+                <picture>
+                    <source media= "(min-width:700px) "srcset="public/images/gallery/Eye_cat_medium.jpg">
+                    <source media="(min-width:960px)" srcset="public/images/gallery/Eye_cat_large.jpg">
+                    <img src="public/images/gallery/Eye_cat_small.jpg" title="Eyes of Cat" alt="Eyes-of-cat">
+                </picture>
+            </a>
+            <div class="desc"><span>Eyes Of A Cat</span>  <br> Dry pastel - Real size</div>
+        </div>
+
+        <div class="border">
+            <a target="_blank" href="fruit_small.jpg">
+                <picture>
+                    <source media= "(min-width:700px) "srcset="public/images/gallery/fruit_medium.jpg">
+                    <source media="(min-width:960px)" srcset="public/images/gallery/fruit_large.jpg">
+                    <img src="public/images/gallery/fruit_small.jpg" title="Red fruits" alt="fruits">
+                </picture>
+            </a>
+            <div class="desc"><span>Red Fruits</span><br> Dry pastel - Real size</div>
+        </div>
+
+        <div class="border">
+            <a target="_blank" href="girl_small.jpg">
+                <picture>
+                    <source media= "(min-width:700px) "srcset="public/images/gallery/girl_medium.jpg">
+                    <source media="(min-width:960px)" srcset="public/images/gallery/girl_large.jpg">
+                    <img src="public/images/gallery/girl_small.jpg" title="Little girl" alt="litte-girl">
+                </picture>
+            </a>
+            <div class="desc"><span>Little Girl</span><br> Dry pastel - Real size</div>
+        </div>
+
+        <div class="border">
+            <a target="_blank" href="leopard_small.jpg">
+                <picture>
+                    <source media= "(min-width:700px) "srcset="public/images/gallery/leopard_medium.jpg">
+                    <source media="(min-width:960px)" srcset="public/images/gallery/leopard_large.jpg">
+                    <img src="public/images/gallery/leopard_small.jpg" title="The gaze of a leopard" alt="gaze-leopard">
+                </picture>
+            </a>
+            <div class="desc"><span>The Gaze Of A Leopard</span><br> Dry pastel - Real size</div>
+        </div>
+    </div>
+
 </section>
